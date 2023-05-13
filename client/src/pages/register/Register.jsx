278b0aa14a6892fc9722bf2caef5e1e99c070975
unknown <marco.luci95@gmail.com>
2023-05-13 16:48:46 -0400

    works through registry on client side

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,42 @@
 import "./Register.scss";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: "",
+  });
+
+  const [err, setErr] = useState(null);
+
+  // setInputs function is called with argument that is a new object that spreads previous state object
+  // updates the property with name attribute of event.target with value of event.target
+
+  // By using the prev parameter to spread the previous state object - code ensures the previous
+  // state is not lost and only the specified property is updated (import because state updates are asynchronus)
+
+  const handleChange = (event) => {
+    setInputs((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  };
+
+  // When we click on the button, we make a register request
+
+  const handleClick = async (event) => {
+    event.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8800/backend/auth/register", inputs);
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
+  console.log(err);
+
   return (
     <div className="register">
       <div className="card">
@@ -21,11 +56,32 @@ const Register = () => {
         <div className="right">
           <h1>Register</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Password" />
-            <input type="password" placeholder="Email" />
-            <input type="text" placeholder="Name" />
-            <button>Register</button>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={handleChange}
+            />
+            {err && err}
+            <button onClick={handleClick}>Register</button>
           </form>
         </div>
       </div>
