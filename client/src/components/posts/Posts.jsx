9c658da08ec9635bf7e960b3d08fbe58be17295d
unknown <marco.luci95@gmail.com>
2023-05-16 01:42:58 -0400

    sets up test posts for user posts

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -6,15 +6,18 @@ import { makeRequest } from "../../axios";
 const Posts = () => {
   const { isLoading, error, data } = useQuery(["posts"], () =>
     makeRequest.get("/posts").then((res) => {
-      res.data;
+      return res.data;
     })
   );
+  console.log(data);
 
   return (
     <div className="posts">
-      {data.map((post) => (
-        <Post post={post} key={post.id} />
-      ))}
+      {error
+        ? "Something went wrong"
+        : isLoading
+        ? "loading"
+        : data.map((post) => <Post post={post} key={post.id} />)}
     </div>
   );
 };
