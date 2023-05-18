28970dab3fca07cb00dc6cd374449149c91fed7a
unknown <marco.luci95@gmail.com>
2023-05-18 01:38:07 -0400

    creates post functionality

---
+++ client/src/components/share/Share.jsx
@@ -0,0 +1,89 @@
+import "./Share.scss";
+import Image from "../../assets//Images/img.png";
+import Map from "../../assets//Images/map.png";
+import Friend from "../../assets//Images/friend.png";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/authContext";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+
+// Query Client is what we wrapped our app.js around with
+// useMutation allows you to easily make mutations from a React component (e.g updating data)
+
+const Share = () => {
+  const [file, setFile] = useState(null);
+  const [desc, setDesc] = useState("");
+
+  const { currentUser } = useContext(AuthContext);
+
+  // Refresh our posts
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (newPost) => {
+      return makeRequest.post("/posts", newPost);
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(["posts"]);
+      },
+    }
+  );
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    mutation.mutate({ desc });
+  };
+  return (
+    <div className="share">
+      <div className="container">
+        <div className="top">
+          <div className="left">
+            <img src={"/upload/" + currentUser.profilePic} alt="" />
+            <input
+              type="text"
+              placeholder={`What's on your mind ${currentUser.name}?`}
+              onChange={(e) => setDesc(e.target.value)}
+            />
+          </div>
+          <div className="right">
+            {file && (
+              <img className="file" alt="" src={URL.createObjectURL(file)} />
+            )}
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <input
+              type="file"
+              id="file"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <label htmlFor="file">
+              <div className="item">
+                <img src={Image} alt="" />
+                <span>Add Image</span>
+              </div>
+            </label>
+            <div className="item">
+              <img src={Map} alt="" />
+              <span>Add Place</span>
+            </div>
+            <div className="item">
+              <img src={Friend} alt="" />
+              <span>Tag Friends</span>
+            </div>
+          </div>
+          <div className="right">
+            <button onClick={handleClick}>Share</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Share;
